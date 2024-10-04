030078a1c7c2d4ef9190ffe79da7cc8483f001bf
kawakami <barcarolle.op60.horowitz@gmail.com>
2024-10-04 23:09:52 +0900

    フロントエンドを実装

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,19 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { RouterProvider } from 'react-router-dom';
+import { router } from './routers';
+import { TodoProvider } from './contexts/TodoContext';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const rootElement = document.getElementById('root');
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+if (rootElement) {
+  ReactDOM.createRoot(rootElement).render(
+    <React.StrictMode>
+      <TodoProvider>
+        <RouterProvider router={router} />
+      </TodoProvider>
+    </React.StrictMode>,
+  );
+} else {
+  console.error('Root element not found');
+}
